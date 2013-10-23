a33e1bbd1f0d6313bc3b3e110d7a000bea8843b6
baui94 <c.bauhofer@gmail.com>
2013-10-23 13:15:44 +0200

    simple collision with many bugs

--- Game/Jump_n_Run/Jump_n_Run/Jump_n_Run/Game1.cs
+++ Game/Jump_n_Run/Jump_n_Run/Jump_n_Run/Game1.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-
+using Jump_n_Run.classes;
 namespace Jump_n_Run
 {
     /// <summary>
@@ -20,6 +20,8 @@ namespace Jump_n_Run
         SpriteBatch spriteBatch;
         Rectangle mainFrame = new Rectangle(0, 0, 800, 600);
         Texture2D background;
+        MoveableObject obj;
+        Texture2D objTex;
 
 
         public Game1()
@@ -51,6 +53,10 @@ namespace Jump_n_Run
             spriteBatch = new SpriteBatch(GraphicsDevice);
             background = Content.Load<Texture2D>("800x600_Pacman");
 
+            objTex = new Texture2D(graphics.GraphicsDevice, 64, 64);
+            objTex = Content.Load<Texture2D>("object");
+            obj = new MoveableObject(10, objTex, new Rectangle(100, 100, 64, 64));
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -70,7 +76,35 @@ namespace Jump_n_Run
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+
+            KeyboardState kbstate = Keyboard.GetState();
+
             // Allows the game to exit
+            if (kbstate.IsKeyDown(Keys.Escape))
+            {
+                this.Exit();
+            }
+
+            if (kbstate.IsKeyDown(Keys.W))
+            {
+               if(!Collision(mainFrame,obj.rectangle,obj.movementSpeed))
+                obj.rectangle.Y -= obj.movementSpeed;
+            }
+            if (kbstate.IsKeyDown(Keys.A))
+            {
+                if (!Collision(mainFrame, obj.rectangle, obj.movementSpeed))
+                obj.rectangle.X -= obj.movementSpeed;
+            }
+            if (kbstate.IsKeyDown(Keys.S))
+            {
+                if (!Collision(mainFrame, obj.rectangle, obj.movementSpeed))
+                obj.rectangle.Y += obj.movementSpeed;
+            }
+            if (kbstate.IsKeyDown(Keys.D))
+            {
+                if (!Collision(mainFrame, obj.rectangle, obj.movementSpeed))
+                obj.rectangle.X += obj.movementSpeed;
+            }
             
 
             // TODO: Add your update logic here
@@ -86,9 +120,31 @@ namespace Jump_n_Run
         {
             spriteBatch.Begin();
             spriteBatch.Draw(background,mainFrame,Color.White);
+            spriteBatch.Draw(obj.Texture, obj.rectangle, Color.White);
             // TODO: Add your drawing code here
             spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        static bool Collision(Rectangle bounds, Rectangle moveable, int moveSpeed)
+        {
+            bool result1;
+            bool result2;
+            bool result3;
+            bool result4;
+
+            Rectangle futureRect1 = new Rectangle(moveable.X + moveSpeed ,moveable.Y, moveable.Width, moveable.Height);
+            Rectangle futureRect2 = new Rectangle(moveable.X - moveSpeed ,moveable.Y, moveable.Width, moveable.Height);
+            Rectangle futureRect3 = new Rectangle(moveable.X , moveable.Y + moveSpeed , moveable.Width, moveable.Height);
+            Rectangle futureRect4 = new Rectangle(moveable.X , moveable.Y - moveSpeed , moveable.Width, moveable.Height);
+
+            bounds.Contains(ref futureRect1, out result1);
+            bounds.Contains(ref futureRect2, out result2);
+            bounds.Contains(ref futureRect3, out result3);
+            bounds.Contains(ref futureRect3, out result4);
+
+
+            return !(result1 & result2 & result3 & result4);
         }
     }
 }
---
+++ Game/Jump_n_Run/Jump_n_Run/Jump_n_Run/classes/MoveableObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Jump_n_Run.classes
+{
+    class MoveableObject
+    {
+        public int movementSpeed;
+        public Texture2D Texture;
+        public Rectangle rectangle;
+       
+
+        public MoveableObject(int moveSpeed, Texture2D texture, Rectangle rect)
+        {
+            this.movementSpeed = moveSpeed;
+            this.Texture = texture;
+            this.rectangle = rect;
+           
+        }
+    }
+}
